a940e5b77cc22ebc0874cef9ccc651a83fa1ca2f
Wnagfeng <2712442320@qq.com>
2024-06-02 22:53:13 +0800

    learn Math

---
+++ 29API（常见API，对象克隆）/Worke/src/Demo.java
@@ -0,0 +1,19 @@
+public class Demo {
+    public static void main(String[] args) {
+//        自幂数：
+//        统计1-999有多少水仙花数。
+
+//        水仙花数是指一个 3 位数，其各位数字的立方和等于该数本身。
+//        例如：153 = 1^3 + 5^3 + 3^3。
+
+
+        for(int i=100;i<1000;i++) {
+            int a = i / 100;
+            int b = (i - a * 100) / 10;
+            int c = i - a * 100 - b * 10;
+            if (a * a * a + b * b * b + c * c * c == i) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ 29API（常见API，对象克隆）/Worke/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+//        判断一个数是否为质数
+        int num = 13;
+        boolean isPrime = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}

21b51d76e9d1ec6b5aa2274565bf22c17b38ffd0
Wnagfeng <2712442320@qq.com>
2024-06-04 16:14:23 +0800

    SearchCode

---
+++ 32Search-Code/SearchCode/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println(binarySearch(array, 7));
+
+    }
+//    二分查找
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+}
